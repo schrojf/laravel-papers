644cb7154a99e32ce2c390475a58f43fc7417385
Viliam Schrojf <viliam@schrojf.sk>
2025-05-23 19:23:03 +0200

    Add support for `once` for Laravel v9 and v10

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,18 @@
 <?php
+
+if (! function_exists('once')) {
+    /**
+     * @template T
+     *
+     * @param (callable(): T) $callback
+     * @return T
+     */
+    function once(callable $callback): mixed
+    {
+        if (function_exists('\Spatie\Once\once')) {
+            return \Spatie\Once\once($callback);
+        }
+
+        return call_user_func($callback);
+    }
+}
