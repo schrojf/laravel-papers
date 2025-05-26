24515305c69a7c9fd8993c6c94c72148f2565a91
Viliam Schrojf <viliam@schrojf.sk>
2025-05-26 18:21:21 +0200

    Command to list all registered papers

--- src/Commands/PapersCommand.php
+++ src/Commands/PapersCommand.php
@@ -3,16 +3,24 @@
 namespace Schrojf\Papers\Commands;
 
 use Illuminate\Console\Command;
+use Schrojf\Papers\Papers;
 
 class PapersCommand extends Command
 {
     public $signature = 'papers';
 
-    public $description = 'My command';
+    public $description = 'List all registered papers';
 
     public function handle(): int
     {
-        $this->comment('All done');
+        $this->comment('Listing registered papers...');
+
+        $papers = Papers::all();
+
+        $this->table(
+            ['Paper Name', 'Url Key', 'Description'],
+            array_map(fn (string $paper) => [$paper::name(), $paper::handler(), $paper::description()], $papers),
+        );
 
         return self::SUCCESS;
     }
